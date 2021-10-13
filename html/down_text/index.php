ece85f0d33bb2c121bf428510afa8f20a941d198
ycMia <zzlyx20123201@126.com>
2021-10-13 11:24:08 +0800

    20210917

--- html/down_text/index.php
+++ html/down_text/index.php
@@ -1,9 +1,18 @@
 <html>
+
+<style>
+p
+{
+	margin: 0 0 0 0;
+	border-style:none;
+}
+</style>
 <?php
+$max_integer = 2147483647;
 function showNormal()
 {
 	echo <<<EOF
-	<body>
+	<body onload="opener.location.reload()">
 <form method="post" target="/down_text/index.php">
 <textarea name="box"></textarea>
 
@@ -14,26 +23,35 @@ function showNormal()
 EOF;
 }
 
-function showResult()
+function showResult($time1)
 {
 	echo <<<EOF
 	<body>
+EOF;
+	echo "<p>";
+	echo $time1;
+	echo "</p>";
+//	echo "</br>";
+	echo <<<EOF
+	<a href="./file/" >file</a>
 	</body>
 EOF;
 }
 
+//echo $_COOKIE["submitted"];
 
-if($_POST["box"])
+if($_POST["box"] && $_COOKIE["submitted"]=="nope")
 {
 	$time1 = time();
 	$file = fopen("./file/$time1.txt","w+");
 	fwrite($file,$_POST["box"]);
 	fclose($file);
-	showResult();	
+	setcookie("submitted",'',time()-1);
+	showResult($time1);
 }
 else
 {
-	
+	setcookie("submitted","nope",$max_integer);
 	showNormal();	
 }
 ?>
